aaad66ed94fec66b4d625a8501dc366a47f2c248
Freaks <galleyhugo@icloud.com>
2024-10-25 22:27:13 +0200

    FIX doaminname

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,12 +3,12 @@ import logo from '../assets/logo.png'
 export default function NavBar(){
     return(
         <nav className='navbar'>
-        <a href="/portfolio/portfolio/#Home"><img className='navbar-logo' src={logo} alt='logo' /></a>
+        <a href="/#Home"><img className='navbar-logo' src={logo} alt='logo' /></a>
           <ul className='navbar-right'>
-            <a href="/portfolio/#Home"><li className='navbar-items'>Home</li></a> 
-            <a href="/portfolio/#AboutMe"><li className='navbar-items'>About</li></a> 
-            <a href="/portfolio/#Projects"><li className='navbar-items'>Projects</li></a> 
-            <a href="/portfolio/#Contact"><li className='navbar-items'>Contact</li></a> 
+            <a href="/#Home"><li className='navbar-items'>Home</li></a> 
+            <a href="/#AboutMe"><li className='navbar-items'>About</li></a> 
+            <a href="/#Projects"><li className='navbar-items'>Projects</li></a> 
+            <a href="/#Contact"><li className='navbar-items'>Contact</li></a> 
             
           </ul>
         </nav>
