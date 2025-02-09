04535be9f7951800022e94b319b880e9c975a000
Freaks <galleyhugo@icloud.com>
2025-02-09 16:16:04 +0100

    Modification du Herro

--- src/components/Herro.jsx
+++ src/components/Herro.jsx
@@ -1,26 +1,26 @@
 import '../Styles/Herro.css'
-import heroImg from'../assets/herroBanner.webp'
 import DownLoadButton from './DownloadButton';
-import ScrollButton from './ScrollButton';
 
 export default function Herro() {
   return (
     <div className='herroBanner'>
       <div className='herro'>
       <div className='herro-description'>
-        <p id='hello'>Hello</p>
-        <p id='herro-name'>Je suis <strong>Hugo Galley</strong></p>
-        <p id='herro-desc'>Je suis un jeune développeur actuellement en études</p>
+      <h1 class="hero-title">
+            Hello,<br/>
+            <span class="gradient-text">Je suis Hugo Galley</span>
+        </h1>
+        
+        <p class="hero-subtitle">
+            Je suis un jeune développeur actuellement en études
+        </p>
+
         <div className='button-div'>
           <a id='herro-button1' href='#AboutMe'>A Propos</a>
           <a id='herro-button2' href='#Projects'>Mon Travail</a>
         </div>
         <a id='DownloadButton' href="https://cvdesignr.com/p/635c197aeaa16?hl=fr_FR" target='_blanck'><DownLoadButton/></a>
         
-      </div>
-      <div>
-      <img src={heroImg} id='herro-logo' alt='Herro'/>
-      <div id='ScrollButton'><ScrollButton/></div> 
       </div>
 
       </div>
