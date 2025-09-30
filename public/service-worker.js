1618439a1d33e9daf287ddaaf4b3909d62266d0c
Freaks <galleyhugo@icloud.com>
2025-09-30 10:35:57 +0200

    fix(cache) : Mise en place des nouveaux assets dans le cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,5 @@
 const CACHE_NAME = 'image-cache-v1';
 const urlsToCache = [
-  '/src/assets/herroBanner.webp',
   '/src/assets/logo.webp',
   '/src/assets/Bento/cs.webp',
   '/src/assets/Bento/epsi.webp',
@@ -11,14 +10,28 @@ const urlsToCache = [
   '/src/assets/Contact/github.webp',
   '/src/assets/Contact/img.icons8.com.webp',
   '/src/assets/Card/actifit.webp',
+  '/src/assets/Card/AXA_Logo.svg-2.png',
+  '/src/assets/Card/azure.webp',
+  '/src/assets/Card/C_Logo.webp',
+  '/src/assets/Card/C_Logo.webp',
+  '/src/assets/Card/cs.webp',
+  '/src/assets/Card/Figma-logo.svg.png',
+  '/src/assets/Card/git.png',
+  '/src/assets/Card/gmailAi.webp',
+  '/src/assets/Card/HTML-CSS-JS-Logo.webp',
+  '/src/assets/Card/linux-black.webp',
+  '/src/assets/Card/linux.webp',
   '/src/assets/Card/Carto.webp',
   '/src/assets/Card/ctk.webp',
   '/src/assets/Card/ctk2.webp',
   '/src/assets/Card/instagram-2.webp',
-  '/src/assets/Card/Logo-BUT.svg.webp',
   '/src/assets/Card/mario.webp',
   '/src/assets/Card/unif.webp',
-  '/src/assets/Card/virus.webp'
+  '/src/assets/Card/virus.webp',
+  '/src/assets/Card/phantom-card.png',
+  '/src/assets/Card/phantom.png',
+  '/src/assets/Card/PyPI-Logo-notest-svg.png',
+  '/src/assets/Card/SyncCRD2CRM.png',
 ];
 
 self.addEventListener('install', (event) => {
